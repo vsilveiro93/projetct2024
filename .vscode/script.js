146112583cb65eadc0a3d81c9e73d153e440e5ad
Victor Silverio <vsilverio93@gmail.com>
2024-01-30 16:56:13 -0300

    first commit

---
+++ .vscode/script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+
+const img = document.querySelector ("#profile img")
+if(html.classList.contains('light')) {
+  img.setAttribute("src", "./assets/assets/lion.png")
+} else {
+   img.setAttribute("src", "./assets/assets/avatar.png")
+}
+
+}
